4851b9a1ba34422fbcdf70bc135fdcf6fe27649a
Sirfilior <66057362+Sirfilior@users.noreply.github.com>
2021-04-13 19:30:00 +0200

    Initial template project

---
+++ controllers/example.js
@@ -0,0 +1,12 @@
+const sequelize = require("../db");
+//const { <MODEL> } = sequelize.models;
+
+async function getAll(req, res) {
+  //Enable for cache testing
+  /*const res = await <MODEL>.findAll({});*/
+  res.status(200);
+}
+
+module.exports = {
+  getAll,
+};
